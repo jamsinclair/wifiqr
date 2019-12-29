528fc205e5174eded90f3ef7d9954b5f83e34403
jamsinclair <jamsinclairnz@gmail.com>
2019-12-29 18:54:29 +0900

    Cross platform that script

--- scripts/update-integrity.sh
+++ scripts/update-integrity.sh
@@ -2,7 +2,11 @@ updateIntegrity () {
   hash=$(cat $1 | openssl dgst -sha384 -binary | openssl base64 -A)
   match="src=\".\/$1\" integrity=\".*\""
   replace="src=\".\/$1\" integrity=\"$hash\""
-  sed -i '' -e "s/$match/$replace/" index.html
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' -e "s/$match/$replace/" index.html
+  else
+    sed -i -e "s/$match/$replace/" index.html
+  fi
 }
 
 updateIntegrity qrcode.min.js
