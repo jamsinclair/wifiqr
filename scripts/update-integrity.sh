ddac6e60ebdd9f649db922e796c8c1814bbf6311
jamsinclair <jamsinclairnz@gmail.com>
2020-02-08 17:36:11 +0900

    Fix integrity attribute generation

--- scripts/update-integrity.sh
+++ scripts/update-integrity.sh
@@ -1,7 +1,7 @@
 updateIntegrity () {
   hash=$(cat $1 | openssl dgst -sha384 -binary | openssl base64 -A)
   match="src=\".\/$1\" integrity=\".*\""
-  replace="src=\".\/$1\" integrity=\"$hash\""
+  replace="src=\".\/$1\" integrity=\"sha384-$hash\""
   if [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i '' -e "s/$match/$replace/" index.html
   else
